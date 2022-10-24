9acddcbab803ed55df11611b16347a862a91022e
Morgan-Mischo <goramorgan@gmail.com>
2022-10-23 20:51:15 -0600

    Initial Commit

---
+++ Recipe App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Recipe App
+//
+//  Created by Morgan Mischo on 10/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Morgan Mischo on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
